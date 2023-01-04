29b5703caebade1a3a69c79785bc90d1ed7e2105
ajospino <ajospinoa@eafit.edu.co>
2023-01-03 19:47:40 -0500

    Tested methods with auxiliary API, added gemfile

--- main.rb
+++ main.rb
@@ -2,13 +2,35 @@ require 'http'
 require 'dotenv'
 Dotenv.load
 
-def main()
-    
+class APIRequest
+    def main()
+        response = getRequest()
+        dataStructure = parseJson(response)
+        postRequest(dataStructure)
+    end
+
+    def getRequest()
+        address = ENV['ADDRESS']
+        host =  ENV['HOST']
+        rapidKey = ENV['RAPID_API_KEY']
+        #secretKey =  ENV['API_KEY']
+        response = HTTP.headers("X-RapidAPI-Key"=>rapidKey, "X-RapidAPI-Host"=>host).get(address, :params =>{"health"=>"5"})
+        puts response.code
+        return response.parse
+    end
+
+    def parseJson(response)
+        #actvities = []
+        response.each do |key,value|
+            puts " Key: #{key}, Value: #{value}"
+        end
+    end
+
+    def postRequest()
+        
+        
+    end
 end
 
-def getRequest()
-    address = ENV['ADDRESS']
-    secretKey =  ENV['API_KEY']
-    response = HTTP.get(address, :params =>{ :api_key => secretKey})
-    puts response.parse
-end
+A = new APIRequest() 
+A.main()
