7a214b38d32069e1295bc8d66b0a2fafb6005369
ajospino <ajospinoa@eafit.edu.co>
2023-01-05 19:46:09 -0500

    Versión con debugging

--- main.rb
+++ main.rb
@@ -5,18 +5,16 @@ Dotenv.load
 
 class APIRequest
     @@address = ENV['ADDRESS']
-    @@host =  ENV['HOST']
-    @@rapidKey = ENV['RAPID_API_KEY']
     @@secretKey =  ENV['API_KEY']
     def main()
         response = getRequest()
         parseJson(response)
-        #dataStructure = parseJson(response)
+        dataStructure = parseJson(response)
         #postRequest(dataStructure)
     end
 
     def getRequest()
-        response = HTTP.headers("X-RapidAPI-Key"=>@@rapidKey, "X-RapidAPI-Host"=>@@host).get(@@address)
+        response = HTTP.auth(@@secretKey).get(@@address)
         puts response.code
         return response.parse
     end
@@ -37,6 +35,12 @@ class APIRequest
                                         "first_seen_at": "2021-09-13T02:41:51.117-04:00"
                                     },
                                     {
+                                        "id": 251953,
+                                        "user_id": "3pyg3scx",
+                                        "answered_at": "2021-09-10T19:22:40.799-04:00",
+                                        "first_seen_at": "2021-09-10T19:22:23.799-04:00"
+                                    },
+                                    {
                                         "id": 37191,
                                         "user_id": "emr5zqid",
                                         "answered_at": "2021-09-14T00:31:36.117-04:00",
@@ -45,10 +49,12 @@ class APIRequest
                                 ]
                     }
         response = activities[:activities]
+        response.sort_by!{|value| value[:first_seen_at]}
         sessions = result[:user_sessions]
         previousUser = " "
         previousTime = 0
         count = -1
+        i = 1
         response.each do |a|
             regisUser = a[:user_id]
             activityId = a[:id]
@@ -64,48 +70,17 @@ class APIRequest
                                         }
                                         ]
                                 )
-                previousTime = activityEnd
-                previousUser = regisUser
-                count+=1
-            else
-                if previousTime != 0
-                    timeDifference = dateCalculator(previousTime, activityStart)
-                    if timeDifference<=300 && regisUser.eql?(previousUser)
-                        currentDuration = sessions[regisUser.to_sym][count][:duration_seconds] + timeDifference
-                        sessions[regisUser.to_sym][count].store(:ended_at, activityEnd)
-                        sessions[regisUser.to_sym][count].store(:duration_seconds, currentDuration) 
-                        sessions[regisUser.to_sym][count][:activity_ids].push(activityId)
-                        previousTime = activityEnd
-                    elsif timeDifference>300 && regisUser.eql?(previousUser)
-                        sessions[regisUser.to_sym].push(
-                                                        {
-                                                        "ended_at": 0,
-                                                        "started_at": activityStart,
-                                                        "activity_ids": [activityId] ,
-                                                        "duration_seconds": 0 
-                                                        }
-                                                    )
-                        previousTime = activityEnd
-                        count+=1
-                    else
-                        if !(previousUser.eql?(regisUser)) && !(sessions.has_key?(regisUser))
-                            sessions.store(regisUser , [
-                                                        {
-                                                        "ended_at": 0,
-                                                        "started_at": activityStart,
-                                                        "activity_ids": [activityId] ,
-                                                        "duration_seconds": 0 
-                                                        }
-                                                    ]
-                                        )
-                            previousTime = activityEnd
-                            previousUser = regisUser
-                            count=0  
-                        end  
+                puts "Usuario nuevo, primer caso."
+                puts sessions
+                if !(previousUser.eql?(regisUser)) && !(previousUser.eql?(" "))
+                    if sessions[previousUser.to_sym][0][:duration_seconds] == 0 && sessions[previousUser.to_sym][0][:ended_at] == 0
+                        currentDuration = dateCalculator(activityStart,activityEnd)
+                        sessions[previousUser.to_sym][count].store(:ended_at, activityEnd)
+                        sessions[previousUser.to_sym][count].store(:duration_seconds, currentDuration)
+                        count=-1
+                        puts "Usuario antiguo y distinto, cerrado por no tener más actividades Primer caso"
                     end
-                end
-                if !(previousUser.eql?(regisUser))
-                    sessions.store(regisUser , [
+                    sessions.store(regisUser.to_sym , [
                                                 {
                                                 "ended_at": 0,
                                                 "started_at": activityStart,
@@ -115,13 +90,94 @@ class APIRequest
                                                 ]
                                     )
                     previousTime = activityEnd
+                    previousUser = regisUser   
+                end 
+                previousTime = activityEnd
+                previousUser = regisUser
+                count+=1
+            else
+                if previousTime != 0
+                    timeDifference = dateCalculator(previousTime, activityStart)
+                    puts timeDifference
+                    if timeDifference<=300 && regisUser.eql?(previousUser)
+                        puts sessions[regisUser.to_sym], count
+                        currentDuration = sessions[regisUser.to_sym][count][:duration_seconds] + timeDifference
+                        sessions[regisUser.to_sym][count].store(:ended_at, activityEnd)
+                        sessions[regisUser.to_sym][count].store(:duration_seconds, currentDuration) 
+                        sessions[regisUser.to_sym][count][:activity_ids].push(activityId)
+                        previousTime = activityEnd
+                        puts "Mismo usuario, misma sesión"
+                    elsif timeDifference>300 && regisUser.eql?(previousUser)
+                        if i == response.length
+                            currentDuration = dateCalculator(activityStart,activityEnd)
+                            sessions[regisUser.to_sym].push(
+                                                            {
+                                                            "ended_at": activityEnd,
+                                                            "started_at": activityStart,
+                                                            "activity_ids": [activityId] ,
+                                                            "duration_seconds": currentDuration
+                                                            }
+                                                            )
+                            puts "Sesión distina mismo usuario y ultima actividad de la lista"
+                            previousTime = activityEnd
+                            count+=1
+                        else
+                            sessions[regisUser.to_sym].push(
+                                                            {
+                                                            "ended_at": 0,
+                                                            "started_at": activityStart,
+                                                            "activity_ids": [activityId] ,
+                                                            "duration_seconds": 0 
+                                                            }
+                                                            )
+                            puts "Sesión distina mismo usuario y una actividad X"
+                            previousTime = activityEnd
+                            count+=1
+                        end
+                    else
+                        if !(previousUser.eql?(regisUser)) && !(sessions.has_key?(regisUser.to_sym))
+                            sessions.store(regisUser.to_sym , [
+                                                        {
+                                                        "ended_at": 0,
+                                                        "started_at": activityStart,
+                                                        "activity_ids": [activityId] ,
+                                                        "duration_seconds": 0 
+                                                        }
+                                                    ]
+                                        )
+                            puts "Sesión distina, usuario nuevo y distinto"
+                            previousTime = activityEnd
+                            previousUser = regisUser
+                            count=-1  
+                        end  
+                    end
+                end
+                if !(previousUser.eql?(regisUser))
+                    if sessions[previousUser.to_sym][count][:duration_seconds] == 0 && sessions[previousUser.to_sym][count][:ended_at] == 0
+                        currentDuration = dateCalculator(activityStart,activityEnd)
+                        sessions[previousUser.to_sym][count].store(:ended_at, activityEnd)
+                        sessions[previousUser.to_sym][count].store(:duration_seconds, currentDuration)
+                        puts "Usuario antiguo y distinto, cerrado por no tener más actividades."
+                    end
+                    sessions.store(regisUser.to_sym , [
+                                                {
+                                                "ended_at": 0,
+                                                "started_at": activityStart,
+                                                "activity_ids": [activityId] ,
+                                                "duration_seconds": 0 
+                                                }
+                                                ]
+                                    )
+                    puts "Usuario nuevo completamente"
+                    previousTime = activityEnd
                     previousUser = regisUser
-                    count=0   
+                    count=-1   
                 end 
             end
-            puts sessions
+            i +=1
         end 
         puts result
+        return result
     end
 
     def dateCalculator(date1, date2)
